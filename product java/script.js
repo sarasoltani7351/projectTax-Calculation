5643796b4b164a326f87a4b6dc9ecbe4bcff3c9f
Hossein Ashrafipoor <ashrafipoor@gmail.com>
2022-02-12 15:11:00 +0100

    follow my comment and refactor your code

--- product java/script.js
+++ product java/script.js
@@ -1,29 +1,48 @@
+//Crete function for area,envairoment,levy like this:
+// function getArea(radius) {
+//     return 3.14 * radius * radius;
+// }
+
+// function getEnvairoment(radius) {
+//     return 2 * 3.14 * radius;
+// }
+
+// function getTax(fee) {
+//     const tax = 0.09;
+//     return fee * tax;
+// }
+
 function area() {
-    // Get the value of the input field with id="numb"
-    let r = document.getElementById("Radius").value;
-    let p = 3.14;
-    // If x is Not a Number or less than one or greater than 10
-    let A;
-    A = (p * r * r);
-    document.getElementById("demo1").innerHTML =  "مساحت  = " + A ;
+    //what is this comment?
+        // Get the value of the input field with id="numb"
+    let r = document.getElementById("Radius").value; // use clear naming like: area & use const instead let for constant value
+    let p = 3.14;// use clear naming & use const instead let for constant value
+    //what is this comment?
+        // If x is Not a Number or less than one or greater than 10
+    let A;//use clear naming like : area
+    A = (p * r * r); // use getArea function : const area = getArea(radius);
+    document.getElementById("demo1").innerHTML = "مساحت  = " + A;
 }
+//use camelCase naming , please read this page : https://www.w3schools.com/js/js_conventions.asp
 function Environment() {
-    // Get the value of the input field with id="numb"
-    let r = document.getElementById("Radius").value;
-    let p = 3.14;
-    // If x is Not a Number or less than one or greater than 10
-    let E;
-    E = 2 * p * r;
+    //what is this comment?
+        // Get the value of the input field with id="numb"
+    let r = document.getElementById("Radius").value;// use clear naming like: area & use const instead let for constant value
+    let p = 3.14;// use clear naming & use const instead let for constant value
+    //what is this comment?
+        // If x is Not a Number or less than one or greater than 10
+    let E;//use clear naming like : area
+    E = 2 * p * r;// use getEnvairoment function : const envairoment = getEnvairoment(radius);
     document.getElementById("demo").innerHTML = "محیط  = " + E;
 }
-function levy(){
-    var Am= document.getElementById("Amount").value;
-    const L = 0.09;
-    var M;
+function levy() {
+    var Am = document.getElementById("Amount").value;// use clear naming like : fee
+    const L = 0.09;// use clear naming 
+    var M;// use clear naming & use getTax function like : const tax = getTax(fee);
     M = Am * L;
+    // you can use declare and define in same line of code for example : const total = Number(fee) + tax;
     var total;
     total = Number(Am) + M;
-    document.getElementById("demo4").innerHTML = " مبلغ با مالیات = " + total + " تومان" ;
-    document.getElementById("demo3").innerHTML = " مالیات بر اساس مبلغ وارد شده = " + M + " تومان" ;
+    document.getElementById("demo4").innerHTML = " مبلغ با مالیات = " + total + " تومان";
+    document.getElementById("demo3").innerHTML = " مالیات بر اساس مبلغ وارد شده = " + M + " تومان";
 }
-    
